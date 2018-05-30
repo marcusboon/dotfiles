c99fb264134377b2dfe765fab2051faef489d738
Marcus Boon <marcus@catalyst-au.net>
2018-05-30 11:20:08 +1000

    Simplify removing of docker volumes since version 1.9

--- dockerclean.sh
+++ dockerclean.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # remove exited containers:
-sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r sudo docker rm -v
 
 # remove unused images:
-sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r sudo docker rmi
 
 # remove unused volumes:
-sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
-  docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
-  ) | xargs -r rm -fr
+sudo docker volume ls -qf dangling=true | xargs -r sudo docker volume rm
