20290b76106f5c69c979ffdd7acaeed97e74ab16
Marcus Boon <marcus@catalyst-au.net>
2019-10-25 16:36:49 +1000

    Update ghetto window management to percentages... sorta

--- windowmgnt/win_active_mid_third.sh
+++ windowmgnt/win_active_mid_third.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+# get width of screen and height of screen
+SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
+SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
+
+# new width and height
+X=$(($SCREEN_WIDTH / 3))
+Y=0
+W=$(($SCREEN_WIDTH / 3))
+H=$(($SCREEN_HEIGHT))
+
 wmctrl -r :ACTIVE: -b remove,maximized_vert
 wmctrl -r :ACTIVE: -b remove,maximized_horz
-wmctrl -r :ACTIVE: -e 0,1146,0,1146,1440
+wmctrl -r :ACTIVE: -e 0,$X,$Y,$W,$H
 wmctrl -r :ACTIVE: -b add,maximized_vert
