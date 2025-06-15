e9a2fc12023ada1c4d46231b4ca6de7fbc444fc8
Marcus Boon <marcus@boon.casa>
2025-06-15 15:59:37 +1000

    Added install file to automating the symlinking

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Install the necessary files
+
+# First lets symlink the zshrc
+ln -sfv ~/.dotfiles/zshrc ~/.zshrc
+
+# Next lets symlink the custom theme
+ln -sfv ~/.dotfiles/omz-themes/babun.zsh-theme ~/.oh-my-zsh/themes/babun.zsh-theme
+
+# Then lets do the gitconfig
+ln -sfv ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+# Then lets symlink the tmux config
+ln -sfv ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+# Finally lets symlink the tmux local config
+ln -sfv ~/.dotfiles/tmux.conf.local ~/.tmux.conf.local
