02f3bbdd6d55d8e07f23dafac28e7024b1d649b3
Marcus Boon <marcus@catalyst-au.net>
2025-07-15 13:48:33 +1000

    Update the enabling local ssh

--- bin/enable-ssh-local.sh
+++ bin/enable-ssh-local.sh
@@ -5,4 +5,4 @@ sudo iptables -A INPUT -p tcp -s 10.10.0.0/16 --dport ssh -j ACCEPT
 
 # Enable TCP forwarding to access local DEV sites
 sudo sed -i -e 's/^AllowTcpForwarding\ no//g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+sudo systemctl restart ssh
--- bin/enable-ssh-tcp-forwarding.sh
+++ bin/enable-ssh-tcp-forwarding.sh
@@ -2,4 +2,4 @@
 
 # Enable TCP forwarding to access local DEV sites
 sudo sed -i -e 's/^AllowTcpForwarding\ no//g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+sudo systemctl restart ssh
