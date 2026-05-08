3df91a112794a0f187d2958720da62634210bb19
Marcus Boon <marcus@catalyst-au.net>
2026-05-08 10:22:18 +1000

    Update the ssh tcp forwarding script to include firewall changes

--- bin/enable-ssh-tcp-forwarding.sh
+++ bin/enable-ssh-tcp-forwarding.sh
@@ -3,3 +3,5 @@
 # Enable TCP forwarding to access local DEV sites
 sudo sed -i -e 's/^AllowTcpForwarding\ no//g' /etc/ssh/sshd_config
 sudo systemctl restart ssh
+sudo ufw allow 22/tcp
+sudo ufw allow 8088/tcp
